b1d78e8b5ee398436317c09b916189b542114f35
lswoo0705 <lswoo0705@gmail.com>
2025-05-13 13:31:06 +0900

    init: 프로젝트 세팅

---
+++ src/test/java/com/example/my_msa_board/MyMsaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_msa_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMsaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
